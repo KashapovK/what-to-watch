887e2e6619514f4d8e92f5aff93ac242a57ee6e6
KashapovK <kashapov.konstantin.hr@gmail.com>
2023-10-20 23:39:37 +0300

    Маршрутизация

--- src/pages/Error Page/error-page.tsx
+++
@@ -1,7 +0,0 @@
-import WelcomeScreen from '../Main/welcome-screen';
-
-function ErrorPage(): JSX.Element {
-  return(
-    <><button onClick={WelcomeScreen}></button><h1>"404 error"</h1></>
-  );
-}
---
+++ src/pages/error-page/error-page.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+const ErrorPage = () => (
+  <><h1>404 Not Found</h1>
+    <Link to="/">Go to Main Page</Link>
+  </>
+);
+
+export default ErrorPage;
